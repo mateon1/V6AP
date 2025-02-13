00fb70d22fc96cdbf2385ba5ef6c6d9154025c3a
Mateon1 <matin1111@wp.pl>
2025-02-13 23:14:52 +0100

    Fix Secret Lab music crash

--- desktop_version/src/Music.cpp
+++ desktop_version/src/Music.cpp
@@ -1254,6 +1254,7 @@ void musicclass::changemusicarea(int x, int y)
     switch (track)
     {
     case -1:
+    case 11: /* Secret Lab */
         /* Don't change music. */
         return;
     case -2:
